b7b5f907fee27c29fe41a36ea3a695ba15714b9d
ArthurHeitmann <art.tec.15@gmail.com>
2022-07-18 10:26:05 +0200

    __init__ now takes in either rb or mrb files and then either compiles or decompiles them

--- __init__.py
+++ __init__.py
@@ -1,21 +1,40 @@
+import os
 import sys
 import time
 
 from mrbParser import RiteFile
 from mrbToRb.mrbToRb import mrbToRb
 
-# file = sys.argv[1] if len(sys.argv) >= 2 else "D:\\delete\\mods\\na\\blender\\extracted\\data002.cpk_unpacked\\core\\nier2blender_extracted\\corehap.dat\\global_ac2e8923_scp.bin"
-file = sys.argv[1] if len(sys.argv) >= 2 else "./examples/while.mrb"
-outfile = sys.argv[2] if len(sys.argv) >= 3 else file + ".rb"
+mrbFiles = file = sys.argv[1:]
+# file = sys.argv[1] if len(sys.argv) >= 2 else "./examples/case.mrb"
+
+def decompileFile(file: str):
+    with open(file, "rb") as f:
+        riteFile = RiteFile(f)
+        codesRes = mrbToRb(riteFile)
+        # print(codesRes.toStr())
+    with open(f"{file}.rb", "wb") as f:
+        f.write(codesRes.toStr().encode("utf-8", "ignore"))
+
+def compileFile(file: str):
+    if os.name == "nt":
+        os.system(f"bins\\windows\\mrbc.exe {file}")
+    elif os.name == "posix":
+        os.system(f"./bins/linux/mrbc.exe {file}")
+    else:
+        print(f"Unknown OS '{os.name}'")
 
 t1 = time.time()
 
-with open(file, "rb") as f:
-    riteFile = RiteFile(f)
-    codesRes = mrbToRb(riteFile)
-    print(codesRes.toStr())
-# with open(outfile, "wb") as f:
-#     f.write(codesRes.toStr().encode("utf-8", "ignore"))
+for file in mrbFiles:
+    if file.endswith(".mrb") or file.endswith("_scp.bin"):
+        print(f"Decompiling {file}")
+        decompileFile(file)
+    elif file.endswith(".rb"):
+        print(f"Compiling {file}")
+        compileFile(file)
+    else:
+        print(f"Unknown file type: {file}")
 
 t2 = time.time()
-print(f"\nTime: {((t2 - t1)*1000):.1f}ms")
+print(f"Time: {((t2 - t1)*1000):.1f}ms")
